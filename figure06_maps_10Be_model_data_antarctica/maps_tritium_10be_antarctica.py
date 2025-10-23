82269d2ef00cdaa89d354c07c087c2d41e09dec9
acauquoin <138624311+acauquoin@users.noreply.github.com>
2025-10-23 13:14:30 +0900

    Update figures with correct mean accu rate

--- figure06_maps_10Be_model_data_antarctica/maps_tritium_10be_antarctica.py
+++ figure06_maps_10Be_model_data_antarctica/maps_tritium_10be_antarctica.py
@@ -61,16 +61,16 @@ ifile_data_loc = cwd+'/site_locations.csv'
 station = np.genfromtxt(ifile_data_loc, unpack=True, skip_header=1, delimiter=";", usecols = (0), dtype='str')
 lat_data, lon_data = np.genfromtxt(ifile_data_loc, unpack=True, skip_header=1, delimiter=";", usecols = (1, 2))
 
-## get 10Be modeled values at stations locations
-#print ('10Be modeled values at stations locations')
-#for i in range(len(lat_data)):
-#    beryllium_flux_modeled_value = cdo.remapnn("lon="+str(lon_data[i])+"_lat="+str(lat_data[i]), input='-selvar,tot_be10 '+ifile_totdep_10be, options='--reduce_dim -f nc', returnArray='tot_be10')
-#    beryllium_conc_modeled_value = cdo.remapnn("lon="+str(lon_data[i])+"_lat="+str(lat_data[i]), input='-selvar,tot_be10_conc '+ifile_totdep_10be_conc, options='--reduce_dim -f nc', returnArray='tot_be10_conc')
-#    accu_modeled_value = cdo.remapnn("lon="+str(lon_data[i])+"_lat="+str(lat_data[i]), input='-selvar,aprt '+ifile_precip, options='--reduce_dim -f nc', returnArray='aprt')
-#    accu_modeled_value = accu_modeled_value * 1000 / 10000 * (60*60*24*365)
-#    elev = cdo.remapnn("lon="+str(lon_data[i])+"_lat="+str(lat_data[i]), input='-selvar,OROMEA '+ifile_jan_surf, options='--reduce_dim -f nc', returnArray='OROMEA')
-#    print ('station, 10Be_conc (1e5 at/g), 10Be_flux (at/m2/s), accu (g/cm2/yr), elevation (m)')
-#    print (station[i], beryllium_conc_modeled_value, beryllium_flux_modeled_value, accu_modeled_value, elev)
+# get 10Be modeled values at stations locations
+print ('10Be modeled values at stations locations')
+print ('station, 10Be_conc (1e5 at/g), 10Be_flux (at/m2/s), accu (g/cm2/yr), elevation (m)')
+for i in range(len(lat_data)):
+    beryllium_flux_modeled_value = cdo.remapnn("lon="+str(lon_data[i])+"_lat="+str(lat_data[i]), input='-selvar,tot_be10 '+ifile_totdep_10be, options='--reduce_dim -f nc', returnArray='tot_be10')
+    beryllium_conc_modeled_value = cdo.remapnn("lon="+str(lon_data[i])+"_lat="+str(lat_data[i]), input='-selvar,tot_be10_conc '+ifile_totdep_10be_conc, options='--reduce_dim -f nc', returnArray='tot_be10_conc')
+    accu_modeled_value = cdo.remapnn("lon="+str(lon_data[i])+"_lat="+str(lat_data[i]), input='-selvar,aprt '+ifile_precip, options='--reduce_dim -f nc', returnArray='aprt')
+    accu_modeled_value = accu_modeled_value * 1000 / 10000 * (60*60*24*365)
+    elev = cdo.remapnn("lon="+str(lon_data[i])+"_lat="+str(lat_data[i]), input='-selvar,OROMEA '+ifile_jan_surf, options='--reduce_dim -f nc', returnArray='OROMEA')
+    print (station[i], beryllium_conc_modeled_value, beryllium_flux_modeled_value, accu_modeled_value, elev)
 
 # Import 10Be data
 ifile_data = cwd+'/mean_10Be_data_holocene.csv'
@@ -92,58 +92,58 @@ center, radius = [0.5, 0.5], 0.5
 verts = np.vstack([np.sin(theta), np.cos(theta)]).T
 circle = mpath.Path(verts * radius + center)
 
-# figure
-fig, ax = plt.subplots(1, 2, figsize=(11.02,  6.35), subplot_kw=dict(projection=ccrs.SouthPolarStereo()), constrained_layout=True, frameon=False)
-plt.rcParams.update({'font.family': 'sans-serif', 'text.usetex': False})
-#
-levels_ps = np.arange(0.1,1.01,0.05)
-n_colors_ps = len(levels_ps) + 1
-cmap = plt.get_cmap('RdYlBu_r', n_colors_ps)
-colors_ps = cmap(range(cmap.N))
-cmap, norm = mc.from_levels_and_colors(levels_ps, colors_ps, extend='both')
-#
-cs = totdep_10be_div_accu.plot.pcolormesh(cmap=cmap, norm=norm, ax=ax[0], transform=ccrs.PlateCarree(), x='lon', y='lat', edgecolors='face', shading='nearest', extend='both', add_colorbar=False)
-ax[0].scatter(lon_data[idx], lat_data[idx], c=flux_data_div[idx], s=100, cmap=cmap, marker='o', edgecolors='k', linewidth=1, norm=norm, zorder=103, transform=ccrs.PlateCarree())
-for i, label in enumerate(station_map):
-    txt = ax[0].annotate(label, (lon_data[i]+lon_offset[i], lat_data[i]+lat_offset[i]), fontsize=14, color='k', fontweight='bold', horizontalalignment=ha_station_map[i], verticalalignment=va_station_map[i], transform=ccrs.PlateCarree(), zorder=600)
-    txt.set_path_effects([path_effects.Stroke(foreground='white', linewidth=2), path_effects.Normal()])
-ax[0].set_extent([-180, 180, -90, -65.5], crs=ccrs.PlateCarree())
-ax[0].coastlines(zorder=101)
-ax[0].text(0.005, 0.995, '(a)', fontsize=18, fontweight='bold', horizontalalignment='left', verticalalignment='top', transform=ax[0].transAxes)
-[x.set_linewidth(0) for x in ax[0].spines.values()]
-#
-cbar=plt.colorbar(cs, orientation="horizontal", ax=ax[0], pad=0.015, extend='both', ticks=levels_ps[::2], aspect=40)
-cbar.ax.tick_params(labelsize=14)
-cbar.set_label('$\mathsf{^{10}}$Be flux / precipitation (10$^5$ at.g$^{-1}$)', size=14, labelpad=7)
-#
-#
-levels_ps = np.arange(0.1,1.91,0.1)
-n_colors_ps = len(levels_ps) + 1
-cmap = plt.get_cmap('RdYlBu_r', n_colors_ps)
-colors_ps = cmap(range(cmap.N))
-cmap, norm = mc.from_levels_and_colors(levels_ps, colors_ps, extend='both')
-#
-cs = totdep_10be_rel.plot.pcolormesh(cmap=cmap, norm=norm, ax=ax[1], transform=ccrs.PlateCarree(), x='lon', y='lat', edgecolors='face', shading='nearest', extend='both', add_colorbar=False)
-ax[1].scatter(lon_data[idx], lat_data[idx], c=flux_data_rel[idx], s=100, cmap=cmap, marker='o', edgecolors='k', linewidth=1, norm=norm, zorder=103, transform=ccrs.PlateCarree())
-for i, label in enumerate(station_map):
-    txt = ax[1].annotate(label, (lon_data[i]+lon_offset[i], lat_data[i]+lat_offset[i]), fontsize=14, color='k', fontweight='bold', horizontalalignment=ha_station_map[i], verticalalignment=va_station_map[i], transform=ccrs.PlateCarree(), zorder=600)
-    txt.set_path_effects([path_effects.Stroke(foreground='white', linewidth=2), path_effects.Normal()])
-ax[1].set_extent([-180, 180, -90, -65.5], crs=ccrs.PlateCarree())
-ax[1].coastlines(zorder=101)
-ax[1].text(0.005, 0.995, '(b)', fontsize=18, fontweight='bold', horizontalalignment='left', verticalalignment='top', transform=ax[1].transAxes)
-[x.set_linewidth(0) for x in ax[1].spines.values()]
-#
-cbar=plt.colorbar(cs, orientation="horizontal", ax=ax[1], pad=0.015, extend='both', ticks=levels_ps[::2], aspect=40)
-cbar.ax.tick_params(labelsize=14)
-cbar.set_label('$\mathsf{^{10}Be\ flux}$ (rel)', size=14, labelpad=7)
+# # figure
+# fig, ax = plt.subplots(1, 2, figsize=(11.02,  6.35), subplot_kw=dict(projection=ccrs.SouthPolarStereo()), constrained_layout=True, frameon=False)
+# plt.rcParams.update({'font.family': 'sans-serif', 'text.usetex': False})
+# #
+# levels_ps = np.arange(0.1,1.01,0.05)
+# n_colors_ps = len(levels_ps) + 1
+# cmap = plt.get_cmap('RdYlBu_r', n_colors_ps)
+# colors_ps = cmap(range(cmap.N))
+# cmap, norm = mc.from_levels_and_colors(levels_ps, colors_ps, extend='both')
+# #
+# cs = totdep_10be_div_accu.plot.pcolormesh(cmap=cmap, norm=norm, ax=ax[0], transform=ccrs.PlateCarree(), x='lon', y='lat', edgecolors='face', shading='nearest', extend='both', add_colorbar=False)
+# ax[0].scatter(lon_data[idx], lat_data[idx], c=flux_data_div[idx], s=100, cmap=cmap, marker='o', edgecolors='k', linewidth=1, norm=norm, zorder=103, transform=ccrs.PlateCarree())
+# for i, label in enumerate(station_map):
+#     txt = ax[0].annotate(label, (lon_data[i]+lon_offset[i], lat_data[i]+lat_offset[i]), fontsize=14, color='k', fontweight='bold', horizontalalignment=ha_station_map[i], verticalalignment=va_station_map[i], transform=ccrs.PlateCarree(), zorder=600)
+#     txt.set_path_effects([path_effects.Stroke(foreground='white', linewidth=2), path_effects.Normal()])
+# ax[0].set_extent([-180, 180, -90, -65.5], crs=ccrs.PlateCarree())
+# ax[0].coastlines(zorder=101)
+# ax[0].text(0.005, 0.995, '(a)', fontsize=18, fontweight='bold', horizontalalignment='left', verticalalignment='top', transform=ax[0].transAxes)
+# [x.set_linewidth(0) for x in ax[0].spines.values()]
+# #
+# cbar=plt.colorbar(cs, orientation="horizontal", ax=ax[0], pad=0.015, extend='both', ticks=levels_ps[::2], aspect=40)
+# cbar.ax.tick_params(labelsize=14)
+# cbar.set_label('$\mathsf{^{10}}$Be flux / precipitation (10$^5$ at.g$^{-1}$)', size=14, labelpad=7)
+# #
+# #
+# levels_ps = np.arange(0.1,1.91,0.1)
+# n_colors_ps = len(levels_ps) + 1
+# cmap = plt.get_cmap('RdYlBu_r', n_colors_ps)
+# colors_ps = cmap(range(cmap.N))
+# cmap, norm = mc.from_levels_and_colors(levels_ps, colors_ps, extend='both')
+# #
+# cs = totdep_10be_rel.plot.pcolormesh(cmap=cmap, norm=norm, ax=ax[1], transform=ccrs.PlateCarree(), x='lon', y='lat', edgecolors='face', shading='nearest', extend='both', add_colorbar=False)
+# ax[1].scatter(lon_data[idx], lat_data[idx], c=flux_data_rel[idx], s=100, cmap=cmap, marker='o', edgecolors='k', linewidth=1, norm=norm, zorder=103, transform=ccrs.PlateCarree())
+# for i, label in enumerate(station_map):
+#     txt = ax[1].annotate(label, (lon_data[i]+lon_offset[i], lat_data[i]+lat_offset[i]), fontsize=14, color='k', fontweight='bold', horizontalalignment=ha_station_map[i], verticalalignment=va_station_map[i], transform=ccrs.PlateCarree(), zorder=600)
+#     txt.set_path_effects([path_effects.Stroke(foreground='white', linewidth=2), path_effects.Normal()])
+# ax[1].set_extent([-180, 180, -90, -65.5], crs=ccrs.PlateCarree())
+# ax[1].coastlines(zorder=101)
+# ax[1].text(0.005, 0.995, '(b)', fontsize=18, fontweight='bold', horizontalalignment='left', verticalalignment='top', transform=ax[1].transAxes)
+# [x.set_linewidth(0) for x in ax[1].spines.values()]
+# #
+# cbar=plt.colorbar(cs, orientation="horizontal", ax=ax[1], pad=0.015, extend='both', ticks=levels_ps[::2], aspect=40)
+# cbar.ax.tick_params(labelsize=14)
+# cbar.set_label('$\mathsf{^{10}Be\ flux}$ (rel)', size=14, labelpad=7)
 
-# show
-plt.show()
+# # show
+# plt.show()
 
-# save
-#fig.savefig(cwd+'/fig06_maps_10Be_model_data_antarctica.pdf', dpi=300)
+# # save
+# #fig.savefig(cwd+'/fig06_maps_10Be_model_data_antarctica.pdf', dpi=300)
 
-#%%
+
 # figure
 fig, ax = plt.subplots(1, 2, figsize=(11.02,  6.35), subplot_kw=dict(projection=ccrs.SouthPolarStereo()), constrained_layout=True, frameon=False)
 plt.rcParams.update({'font.family': 'sans-serif', 'text.usetex': False})
